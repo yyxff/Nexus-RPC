07af8b488903030b332417eaf2ab69ab457d26a7
yyxff <salmontwosday@gmail.com>
2025-07-26 17:17:46 +0800

    Done SerializerJDK implementation

---
+++ rpc-core/src/main/java/com/gitub/yyxff/nexusrpc/core/serializers/SerializerJDK.java
@@ -0,0 +1,37 @@
+package com.gitub.yyxff.nexusrpc.core.serializers;
+
+import com.gitub.yyxff.nexusrpc.core.Serializer;
+
+import java.io.*;
+
+public class SerializerJDK implements Serializer {
+
+    /**
+     * |baos| is a stream like a bytes buffer
+     * |oos| can convert an object into a stream in bytes
+     * So we need to wrap |baos| by |oos|
+     * @param obj: the object to be serialized
+     * @return serialized object in bytes
+     * @param <T>
+     */
+    @Override
+    public <T> byte[] serialize(T obj) {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(baos)){
+            oos.writeObject(obj);
+            return baos.toByteArray();
+        }catch (IOException e){
+            throw new RuntimeException("SerializerJDK error when serializing: ", e);
+        }
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> clazz) {
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+             ObjectInputStream ois = new ObjectInputStream(bais)){
+            return clazz.cast(ois.readObject());
+        }catch (IOException | ClassNotFoundException e){
+            throw new RuntimeException("SerializerJDK error when deserializing: ", e);
+        }
+    }
+}
