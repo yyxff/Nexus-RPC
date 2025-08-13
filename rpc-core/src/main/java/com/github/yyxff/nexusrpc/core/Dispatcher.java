41d8dbf27b20248d8b3a771d4ccf43319912a956
yyxff <salmontwosday@gmail.com>
2025-08-13 22:02:42 +0800

    Added more comments

--- rpc-core/src/main/java/com/github/yyxff/nexusrpc/core/Dispatcher.java
+++ rpc-core/src/main/java/com/github/yyxff/nexusrpc/core/Dispatcher.java
@@ -15,10 +15,21 @@ public class Dispatcher {
     private final Map<String, CompletableFuture<RpcResponse>> idToResult = new ConcurrentHashMap<>();
     private static final Logger logger = Logger.getLogger(Dispatcher.class.getName());
 
+    /**
+     * Register a result future to map by |id|
+     * It should be completed and removed when response is ready
+     * @param id
+     * @param future
+     */
     public void registerFuture(String id, CompletableFuture<RpcResponse> future) {
         idToResult.put(id, future);
     }
 
+    /**
+     * Complete a future with |response| by |id|, then remove it from map
+     * @param id
+     * @param response
+     */
     public void complete(String id, RpcResponse response) {
         if (!idToResult.containsKey(id)) {
             throw new IllegalStateException("No such id: " + id);
