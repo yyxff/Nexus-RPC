fe6bde20561e0e8c2d29147f0cbc98a50b22680d
yyxff <salmontwosday@gmail.com>
2025-08-15 20:22:06 +0800

    Added more comments

--- rpc-common/src/main/java/com/github/yyxff/nexusrpc/common/utils/ConfigLoader.java
+++ rpc-common/src/main/java/com/github/yyxff/nexusrpc/common/utils/ConfigLoader.java
@@ -6,6 +6,9 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.File;
 
 public class ConfigLoader {
+    /*
+     * Read a config as RpcConfig instance from |path|
+     */
     public static RpcConfig load(String path) throws Exception {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         return mapper.readValue(new File(path), RpcConfig.class);
